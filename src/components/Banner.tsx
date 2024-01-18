f65e4bccc08e20606440fe517b24a428f62c4111
jin <jin337x@outlook.com>
2024-01-18 14:32:48 +0800

    update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,7 +5,7 @@ import { memo } from 'react';
 const Banner = memo(({ url }: { url?: string }) => {
   return (
     <div className='relative w-full h-36 sm:h-44 md:h-52 select-none'>
-      {url && <Image className='object-cover' src={url} fill alt='background' />}
+      {url && <Image className='object-cover' src={url} priority fill alt='background' />}
     </div>
   );
 });
