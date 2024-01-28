f576154dd5a82274a6fcdd91ff91ea90cff183ac
jin <jin337x@outlook.com>
2024-01-28 19:20:16 +0800

    update

--- src/hooks/i18n.tsx
+++ src/hooks/i18n.tsx
@@ -3,7 +3,7 @@ import { useAppSelector } from '@/hooks/redux';
 const useLocal = (key: string) => {
   const languageType = useAppSelector(state => state.common.languageType);
   const i18nContent = useAppSelector(state => state.common.i18nContent[languageType]);
-  return i18nContent ? i18nContent[key] || key : key;
+  return i18nContent ? i18nContent[key] || ' ' : ' ';
 };
 
 export { useLocal };
