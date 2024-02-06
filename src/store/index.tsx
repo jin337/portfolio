f7d6821b6010a2ecbf6d1375bcec223cffb2fce1
jin <jin337x@outlook.com>
2024-02-06 20:30:28 +0800

    update

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,14 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import commonReducer from '@/store/reducers/common';
 
+
 export const store = configureStore({
   reducer: {
     common: commonReducer
   },
 });
-
-// 定义根 state 类型
-export type RootState = ReturnType<typeof store.getState>;
-
-// 定义 dispatch 类型
-export type AppDispatch = typeof store.dispatch;
